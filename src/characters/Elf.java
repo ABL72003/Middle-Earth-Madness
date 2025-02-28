7ad7af574001b98bd6b0f7320edee0664bdebbf9
Logan Anderson <ABL72003@gmail.com>
2025-02-27 19:43:23 -0600

    Wrote MiddleEarthCharacter and implemented Elf class. Will copy and tweak for other classes.

--- src/characters/Elf.java
+++ src/characters/Elf.java
@@ -1,5 +1,37 @@
 package characters;
 
-public class Elf {
+public class Elf extends MiddleEarthCharacter {
 
+	public Elf(String name, double health, double power) {
+		//using super constructor from slides in class
+		super(name, health, power);
+	}
+/**
+ * This override checks if Elf is attacking an Orc
+ * Elf gets 1.5 power when doing so
+ */
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		//1.5 damage
+		if (target.getRace().equals("Orc")) {
+			target.health -= this.power * 1.5;
+			return true;
+		}
+		//no damage
+			else if(target.getRace().equals("Dwarf") || target.getRace().equals("Elf")) {
+			return false;
+		}
+		//standard damage
+			else{
+				target.health -= this.power;
+				return true;
+			}
+	}
+
+	@Override
+	public String getRace() {
+		return "Elf";
+	}
+	
+	
 }
--- src/characters/MiddleEarthCharacter.java
+++ src/characters/MiddleEarthCharacter.java
@@ -2,4 +2,46 @@ package characters;
 
 public abstract class MiddleEarthCharacter {
 
+	String name;
+	double health;
+	double power;
+	/**
+	 * constructor with no input
+	 */
+	MiddleEarthCharacter() {
+		this.name = "null";
+		this.health = 0.0;
+		this.power = 0.0;
+	}
+	/**
+	 * constructor with parameters
+	 * @param name
+	 * @param health
+	 * @param power
+	 */
+	MiddleEarthCharacter(String name, double health, double power) {
+		
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	/**
+	 * this will be defined individually in the character classes but cannot be defined here
+	 * @param target
+	 * @return
+	 */
+	abstract boolean attack(MiddleEarthCharacter target);
+	/**
+	 * same with this one
+	 * @return
+	 */
+	abstract String getRace();
+	/**
+	 * display the info of the character
+	 */
+	void displayInfo() {
+		System.out.println("name = " + name);
+		System.out.println("health = " + health);
+		System.out.println("power = " + power); 
+	}
 }
